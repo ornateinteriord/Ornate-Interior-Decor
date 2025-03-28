bb593367e9c295b9a03cfbf49a8e31206d31a35a
sid-1974 <sidgithub1974@gmail.com>
2025-03-28 15:15:33 +0530

    Added Map

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -98,7 +98,7 @@ const Contact = () => {
       
       <section className="h-[500px] w-full">
         <iframe 
-          src="https://maps.app.goo.gl/sbbznmyWpzPJNUfXA" 
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.041401723144!2d77.53727429999999!3d12.9692026!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae3dde6dcd1763%3A0xfd2c0043be1b3327!2sOrnate%20Interior%20Decor%20Pvt%20Ltd!5e0!3m2!1sen!2sin!4v1743154910866!5m2!1sen!2sin" 
           width="100%" 
           height="100%" 
           style={{ border: 0 }} 
